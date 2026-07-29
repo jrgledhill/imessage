c3e73c05670d369c5517665da9baaa526e9b78d0
Your Name <you@example.com>
2026-07-29 10:44:37 -0600

    feat(index.js): import cron job for scheduled tasks

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,6 +10,7 @@ import { clerkMiddleware } from '@clerk/express';
 
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
+import job from "./lib/cron.js";
 
 const app = express();
 const PORT = process.env.PORT
